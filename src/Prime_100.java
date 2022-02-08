56aa8a4956ecb365b91d5065267727c1cc163763
ThietNN <nguyenthiet1225@gmail.com>
2022-02-08 12:05:49 +0700

    new pj

---
+++ src/Prime_100.java
@@ -0,0 +1,13 @@
+public class Prime_100 {
+    public static void main(String[] args) {
+        for (int num=2 ; num<100 ; num++ ){
+            boolean check = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0)
+                    check = false;
+            }
+            if (check)
+                System.out.println(num);
+        }
+    }
+}
